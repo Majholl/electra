cf3e47cb3e75e0ac9a18abc9c1c59131893a4268
Majholl <mohammadreza1231380@gmail.com>
2025-11-02 20:36:19 +0330

    candidate logic added

--- core_apps/candidate/urls.py
+++ core_apps/candidate/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path 
-from ..candidate.views import addnewcandidate, submitcandidate, show_candidate_list
+from ..candidate.views import addnewcandidate, submitcandidate, show_candidate_list, load_selected_candidate
 
 
 
 urlpatterns = [
     path('candidates', show_candidate_list, name='candidatelist'),
-    path('candidate', addnewcandidate, name='AddNewCandidate'),
-    path('submitcandidate', submitcandidate, name='SubmitCandidate')
+    
+    
+    path('candidate/new', addnewcandidate, name='AddNewCandidate'),
+    path('submitcandidate', submitcandidate, name='SubmitCandidate'),
+    
+    
+    path('candidate/<int:id>/', load_selected_candidate, name='ModifySelectedCandidate')
+    
+    
 ]
--- core_apps/candidate/views.py
+++ core_apps/candidate/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.template.loader import render_to_string
+from django.contrib import messages
 from ..user.views import user_data
 
 
@@ -12,9 +13,8 @@ from ..candidate.models import CandidateModel
 
 def show_candidate_list(request):
     try :
-        votepanel = VotePanelModel.objects.filter(created_by = request.user)
-        
-        content_html = render_to_string('admin/candidate/candidate_list-page.html', context={'obj_list':votepanel}, request=request)
+        candidates = CandidateModel.objects.filter(created_by = request.user).all()
+        content_html = render_to_string('admin/candidate/candidate_list-page.html', context={'obj_list':candidates}, request=request)
         return render(request, template_name='admin/admindash.html', context={** user_data(request), 'content':content_html})
     
     except Exception as err:
@@ -31,27 +31,73 @@ def show_candidate_list(request):
 
 
 
-
-
 def addnewcandidate(request):
-    votepanel = VotePanelModel.objects.all()
-    content_html = render_to_string('admin/candidate/candidate.html', context={'obj_list':votepanel}, request=request)
-    return render(request, template_name='admin/admindash.html', context={** user_data(request), 'content':content_html})
+    try:
+        
+        content_html = render_to_string('admin/candidate/addcandidate.html', context={}, request=request)
+        return render(request, template_name='admin/admindash.html', context={** user_data(request), 'content':content_html})
+    
+    except Exception as err:
+        print(err)
+        return redirect(reverse('404-nf'))
 
 
 
 
 
 
+# //TODO check fields && make logic better
 
 
 def submitcandidate(request):
+    try:
+        
+        if (len(request.POST['name'] ) or len(request.POST['description'] ) or len(request.POST['image'] )) == 0 :
+            messages.add_message(request, messages.INFO, 'فیلد ها نباید خالی باشد')
+            return redirect(reverse('AddNewCandidate'))
+        
+        newcandidate = CandidateModel.objects.create(name= request.POST['name'], description = request.POST['description'],
+                                                    image = request.FILES.get('image'), 
+                                                    created_by = request.user,)
+                                            
+        return redirect(reverse('candidatelist'))
+
+    except Exception as err:
+        print(err)
+        return redirect(reverse('404-nf'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def load_selected_candidate(request, id):
+    try:
+        
+        candidate = CandidateModel.objects.get(id = id)
+        context = {'obj_list':candidate}
+        content_template = 'admin/candidate/modify-selected-candidate.html'
+        content_html = render_to_string(content_template, context=context, request=request)
+        
+        return render(request, template_name='admin/admindash.html', context={** user_data(request), 'content':content_html})
+
     
-    
-    newcandidate = CandidateModel.objects.create(name= request.POST['name'], description = request.POST['description'],
-                                                 image = request.FILES.get('image'), 
-                                                 created_by = request.user,)
-    votepanel = [VotePanelModel.objects.get(id = request.POST['panel'])]
-    newcandidate.votepanel.set(votepanel)
-                                        
-    return redirect(reverse('candidatelist'))
+    except Exception as err:
+        print(err)
+        return redirect(reverse('404-nf'))
+
+
+
+
+
+
+
